e529685f9127ae99f6d341fb8eb4ac03132c804d
Joshua Holloway <josh.holloway@live.co.uk>
2018-01-21 21:43:33 +0000

    updated

--- directory.rb
+++ directory.rb
@@ -4,9 +4,9 @@ def input_students
 puts "Please enter the names and the cohorts of the students"
 puts "To finish, just hit return twice"
 
-name = gets.strip
+name = STDIN.gets.chomp
 puts "Can I have the cohort please"
-cohort = gets.strip
+cohort = STDIN.gets.chomp
 
   until name.empty? do
   @students << {name: name, cohort: cohort, hobbies: :football, country_of_birth: :uzbekistan, height: :ten}
@@ -15,20 +15,18 @@ cohort = gets.strip
   elsif puts "Now we have #{@students.count} students"
   end
 
-  name = gets.strip
+  name = STDIN.gets.chomp
     if name != ""
     puts "Can I have the cohort please"
-    cohort = gets.strip
+    cohort = STDIN.gets.chomp
     end
   end
 end
 
 def interactive_menu
-
   loop do
-
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -37,6 +35,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 
 end
@@ -44,9 +43,42 @@ end
 def show_students
 
   print_header
-  print_students_list
+  print_student_list
   print_footer
 
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 def process(selection)
@@ -58,6 +90,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -76,10 +110,11 @@ def print_student_list
   end
 end
 
-def print_footer(names)
+def print_footer
   puts "-------------".center(40)
-  puts "Overall, we have #{names.count} great students".center(40)
+  puts "Overall, we have #{@students.count} great students".center(40)
+  puts " "
 end
 
-
+try_load_students
 interactive_menu
